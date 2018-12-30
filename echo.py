cd9fb59667daae2e34c0e887faf1e948de98e7ab
Harshitadd <31439716+harshitadd@users.noreply.github.com>
2018-12-30 22:22:15 +0530

    Final echo.py

--- echo.py
+++ echo.py
@@ -3,10 +3,9 @@ import time
 if __name__ == "__main__":
     ser = serial.Serial("COM4", 9600, timeout=0.005)
     while True:
-        if(ser.isOpen()):
-            user_input = input("Enter key: ")   #asking user for the character to be sent
-            ########## ENTER YOUR CODE HERE ############
-            ser.write(user_input.encode())      #sending the character
+        if(ser.isOpen()):                       #Checking if input port is open ie capable of communication
+            user_input = input("Enter key: ")   #Taking User input            
+            ser.write(user_input.encode())      #Writing binary data onto the serial port 
             time.sleep(1)                       #letting the atmega recieve the character and send it back
 
             rec = ser.read(13)                  #recieving upto 13 characters sent by atmega  
@@ -14,5 +13,4 @@ if __name__ == "__main__":
 
 
 
-        ############################################
-
+        
