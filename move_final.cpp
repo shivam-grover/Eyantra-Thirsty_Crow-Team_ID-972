8a74571072794baf138c15223c1696250211133f
Shivam Grover <35850682+shivumgrover@users.noreply.github.com>
2019-01-02 14:10:25 +0530

    Update move_final.cpp

--- move_final.cpp
+++ move_final.cpp
@@ -27,20 +27,18 @@ void motor_pin_config()
 
 void timer5_init()
 {
-	TCCR5B = 0x00;		//Stop
-	TCNT5H = 0xFF;		//Counter higher 8-bit value to which OCR5xH value is compared with
-	TCNT5L = 0x01;		//Counter lower 8-bit value to which OCR5xH value is compared with
-	OCR5AH = 0x00;		//Output compare register high value for Left Motor
-	OCR5AL = 0xFF;		//Output compare register low value for Left Motor
-	OCR5BH = 0x00;		//Output compare register high value for Right Motor
-	OCR5BL = 0xFF;		//Output compare register low value for Right Motor
-	OCR5CH = 0x00;		//Output compare register high value for Motor C1
-	OCR5CL = 0xFF;		//Output compare register low value for Motor C1
-	TCCR5A = 0xA9;		/*{COM5A1=1, COM5A0=0; COM5B1=1, COM5B0=0; COM5C1=1 COM5C0=0}
- 					  For Overriding normal port functionality to OCRnA outputs.
-				  	  {WGM51=0, WGM50=1} Along With WGM52 in TCCR5B for Selecting FAST PWM 8-bit Mode*/
+	TCCR5B = 0x00;		
+	TCNT5H = 0xFF;		
+	TCNT5L = 0x01;		
+	OCR5AH = 0x00;		//Setting high value for Output compare register for Left Motor
+	OCR5AL = 0xFF;		//Setting low value for Output compare register for Left Motor
+	OCR5BH = 0x00;		//Setting high value for Output compare register for Right Motor
+	OCR5BL = 0xFF;		//Setting low value for Output compare register for Right Motor
+	OCR5CH = 0x00;		
+	OCR5CL = 0xFF;		
+	TCCR5A = 0xA9;		
 	
-	TCCR5B = 0x0B;	//WGM12=1; CS12=0, CS11=1, CS10=1 (Prescaler=64)
+	TCCR5B = 0x0B;	
 }
 
 void velocity (unsigned char left_motor, unsigned char right_motor)		//a function to set the velocity of the two motors. It accepts input from 0 to 255.
@@ -129,11 +127,11 @@ int main(void)
 		forward();
 		_delay_ms(3000);
 		stop();
-		//magnet_on();
+		magnet_on();
 		_delay_ms(3000);
 		backward();
 		_delay_ms(3000);
-		//magnet_off();
+		magnet_off();
 		stop();
 		_delay_ms(3000);
     }
